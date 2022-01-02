78764d1411557739c71782b435abff588a64583f
Sumanta17 <sumanta17samdder@gmail.com>
2022-01-02 16:19:15 +0000

    Project 1 Review Started

--- src/test/java/com/cybertek/test/CherCherTest.java
+++ src/test/java/com/cybertek/test/CherCherTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -15,7 +16,6 @@ import java.util.concurrent.TimeUnit;
 
 public class CherCherTest {
     WebDriver driver;
-
     WebDriverWait wait;
 
 
@@ -44,9 +44,27 @@ public class CherCherTest {
         // if you don't wait you will get NoAlertPresentException: no such alert
         Alert alert = driver.switchTo().alert();
         alert.accept();
+    }
+
+    @Test
+    public void DisabledButtonTest(){
+        WebElement button = driver.findElement(By.id("disable"));
+        System.out.println("button.isEnabled() = " + button.isEnabled()); //false
+
+        WebElement buttoninnitiator = driver.findElement(By.id("enable-button"));
+        buttoninnitiator.click();
+
+        wait = new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.elementToBeClickable(button));
+
+        System.out.println("button.isEnabled() = " + button.isEnabled());
+        Assert.assertTrue(button.isEnabled(),"verify that button is enable");
+
+        button.click();
+
+
 
 
     }
-
 
 }

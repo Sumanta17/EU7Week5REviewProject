e038e739574a742ef62efc323d996391d144a21c
Sumanta17 <sumanta17samdder@gmail.com>
2022-01-02 15:44:11 +0000

    Project 1

--- src/test/java/com/cybertek/test/CherCherTest.java
+++ src/test/java/com/cybertek/test/CherCherTest.java
@@ -1,28 +1,49 @@
 package com.cybertek.test;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class CherCherTest {
     WebDriver driver;
+
+    WebDriverWait wait;
 
 
     @BeforeMethod
     public void setUp(){
         driver = WebDriverFactory.getDriver("chrome");
 
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://chercher.tech/practice/explicit-wait-sample-selenium-webdriver");
     }
 
     @AfterMethod
     public void tearDown(){
+        driver.close();
 
 
     }
     @Test
     public void alertpresenttest1(){
+        WebElement initiateALert = driver.findElement(By.id("alert"));
+        initiateALert.click();
+        wait = new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.alertIsPresent());
+        // handle javascript alert
+        // if you don't wait you will get NoAlertPresentException: no such alert
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
 
 
     }

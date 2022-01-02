2a103d464d3c4c7122647c93e1533b81a99f69c2
Sumanta17 <sumanta17samdder@gmail.com>
2022-01-02 16:35:32 +0000

    Task3 startes

---
+++ src/test/java/com/cybertek/test/DropDownTest.java
@@ -0,0 +1,64 @@
+package com.cybertek.test;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class DropDownTest {
+
+    WebDriver driver;
+    WebDriverWait wait;
+
+
+    @BeforeMethod
+    public void setUp(){
+        driver = WebDriverFactory.getDriver("chrome");
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(" http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+    }
+
+    @AfterMethod
+    public void tearDown(){
+   //     driver.close();
+
+
+    }
+
+    @Test
+    public void test1 (){
+
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test");
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
+
+        WebElement orderbutton = driver.findElement(By.linkText("Order"));
+        orderbutton.click();
+
+        String expectedselectedoption = "MyMoney";
+        WebElement productdropdownelement = driver.findElement(By.id("ctl00_MainContent_fmwOrder_ddlProduct"));
+
+        Select productDropDown = new Select(productdropdownelement);
+        String actualselectedoptions = productDropDown.getFirstSelectedOption().getText();
+
+        Assert.assertEquals(actualselectedoptions,expectedselectedoption,"First option selected aint expected");
+
+
+
+    }
+
+
+
+
+
+}
